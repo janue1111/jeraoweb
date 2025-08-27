d4de5b3ccd166f835a24d604a65e3be990fefb3d
janue1111 <206374324+janue1111@users.noreply.github.com>
2025-08-27 12:33:28 -0500

    instalar gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from 'next';
 import { Manrope } from 'next/font/google';
 import { Suspense } from 'react';
 import { GtmPageView } from '@/components/GtmPageView';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Next.js SaaS Starter',
@@ -25,7 +26,24 @@ export default function RootLayout({
       lang="en"
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
     >
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MR2FJHHJ');`}
+        </Script>
+      </head>
       <body className="min-h-[100dvh] bg-gray-50">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MR2FJHHJ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Suspense>
           <GtmPageView />
         </Suspense>
